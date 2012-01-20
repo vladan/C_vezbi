9e4a70bbb845be01f303d240aa9d2448d7ca9441
Vladan Popovic <vladanovic@gmail.com>
2012-01-20 19:31:59 +0100

    Додавање на низи

---
+++ nizi/duplikati.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( int argc, char *argv[] )
+{
+    int n, i, j, izbrisani = 0, a[100];
+
+    printf("Vnesi ja dolzinata: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        printf("a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n - 1 - izbrisani; i++)
+    {
+        for(j = i + 1; j < n - izbrisani; j++)
+        {
+            if(a[i] == a[j])
+            {
+                int pos;
+                for(pos = j; pos < n - 1 - izbrisani; pos++)
+                {
+                    a[pos] = a[pos + 1];
+                }
+                izbrisani++;
+            }
+        }
+    }
+
+    for(j = 0; j < n - izbrisani - 1; j++)
+    {
+        printf("%d,", a[j]);
+    }
+    printf("%d", a[j+1]);
+
+    return EXIT_SUCCESS;
+}
